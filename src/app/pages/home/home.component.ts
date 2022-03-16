88f297e015783f0dae75759b559e6d69d2250d13
eric soucy <eric.soucy@gmail.com>
2022-03-15 20:55:14 -0400

    create service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  foodData!: {
+    id: number;
+    foodName: string;
+    foodDetails: string;
+    foodPrice: number;
+    foodImg: string;
+  }[];
 
-  constructor() { }
+  constructor(private service: OrderDetailsService) {}
 
   ngOnInit(): void {
+    this.foodData = this.service.foodDetails;
   }
-
 }
